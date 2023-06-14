232e7cc2dbc3d1884dc84f84eb92ed1bea81d6e9
MartinDeluca2002 <136609125+MartinDeluca2002@users.noreply.github.com>
2023-06-14 11:17:09 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,20 @@
+document.getElementById('menu').addEventListener('click', function
+(){
+    
+    document.getElementById('navega').classList.toggle('mostrar');
+
+/*el menu de hamburguesa no se cierra cuando se expande por que no pude hacer la parte del if y busque maneras de hacerlo pero son con otro codigo y me queria apegar al codigo visto en clase*/
+
+})
+
+
+    lightbox.option({
+      'wrapAround': true,
+      'albumLabel': "Imagen %1 de %2",
+      'fadeDuration': 1000,
+      'maxWidth':1150,
+      'maxHeight':1150
+      
+    })
+    
+    
